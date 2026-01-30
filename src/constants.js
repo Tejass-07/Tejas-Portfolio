7ac1b83229f92915b27ab83eb34beba290143e30
Tejass-07 <tejassshir60@gmail.com>
2026-01-30 18:53:35 +0530

    Change project from Email Validator to Currency Converter
    Updated project details for the email validator to a currency converter web app, including title and description changes.

--- src/constants.js
+++ src/constants.js
@@ -241,9 +241,9 @@ export const SkillsInfo = [
     },
     {
       id: 3,
-      title: "Email Validator NPM Package",
+      title: "Currency Converter Web App",
       description:
-        "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+       "A modern and responsive currency converter application that enables real-time conversion between global currencies. Built using React.js and Tailwind CSS, the app integrates a reliable free exchange-rate API to fetch live data, dynamically updates country flags, and delivers fast, user-friendly conversions. Deployed on Vercel for seamless performance."
       image: npmLogo,
       tags: ["React JS", "Node.js", "NPM", "Validation"],
       github: "https://github.com/codingmastr/cmtk-email-validator",
